8c4790fc2fa63766544943fafcd75c2a8ccc67f1
Daniel R. Koris <korisd@gmail.com>
2017-08-05 18:10:41 -0500

    continuing to build up the outline for the timedqueue state

--- TimedQueueState.h
+++ TimedQueueState.h
@@ -7,13 +7,26 @@ class TimedQueueState : public State
 {
     public:
         TimedQueueState( std::string id = "timed queued state", unsigned long long &t ) : timer(t), State( id ) {}
+        /*
+         * Either onEnter or onExit should most likely reset any events remaining
+         * in the queue. Events should be initialized with an offset based on a
+         * current time.
+         */
         virtual void onEnter( std::string prev_state ) {}
         virtual void onExit( std::string next_state ) {}
+        /*
+         * should likely transition out if its queue is empty
+         */
         virtual std::transition( void ) { return state_identifier; }
+        /*
+         * Action will not be a virtual function because it should handle events,
+         * ie, function calls, in a single uniform manner. 
+         */
         void action()
         {
         }
     private:
+        //event queue
         unsigned long long &timer;
 };
 
