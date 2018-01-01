233c0d04d2d9e6dec7c4be0b4055566f65d027ce
Daniel R. Koris <korisd@gmail.com>
2018-01-01 17:02:42 -0500

    made sure to check for null stuff...

--- IOType.cpp
+++ IOType.cpp
@@ -6,7 +6,8 @@ VALIDATOR ioflt_validator = { IOTYPE_FLT, sizeof( float ), SUM_FLT };
 bool is_io_valid( const IOType *to_test, VALIDATOR params ) {
     bool valid = false;
 
-    if( to_test->type == params.type &&
+    if( to_test &&
+        to_test->type == params.type &&
         to_test->size == params.size &&
         to_test->validating_checksum == params.checksum )
         valid = true;
