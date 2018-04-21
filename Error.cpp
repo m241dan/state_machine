21cc43859d7db92eac5d117c7cefb1d73ae3ecd2
Daniel R. Koris <korisd@gmail.com>
2018-04-21 15:05:50 -0400

    fixed a typo

--- Error.cpp
+++ Error.cpp
@@ -5,7 +5,7 @@ void messaging::errorMsg( std::string func, std::string msg )
     std::cout << "Function::" << func << ": " << msg << std::endl;
 }
 
-void message::successMsg( std::string func, std::string msg )
+void messaging::successMsg( std::string func, std::string msg )
 {
     std::cout << "Function::" << func << ": " << msg << std::endl;
 }
